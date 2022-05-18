ab4550d27e1cda846d76c79a1c2a4362e6b2890f
Alejo Boga <alejoboga20@gmail.com>
2022-05-18 12:31:41 -0300

    Handle Task not found exception for delete and update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -69,6 +69,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const task = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== task.id);
   }
 }
