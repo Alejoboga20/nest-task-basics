3e6acd1079491cc4b28d2ab6431d3062c6fc1dbe
Alejo Boga <alejoboga20@gmail.com>
2022-05-18 12:01:40 -0300

    Update Task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -35,6 +35,13 @@ export class TasksService {
     return task;
   }
 
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status.toUpperCase() as TaskStatus;
+
+    return task;
+  }
+
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
