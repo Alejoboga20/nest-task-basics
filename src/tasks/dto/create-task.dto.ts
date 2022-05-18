daf638d54b2d735e34f4ba6b9faaa6af7fb6fb58
Alejo Boga <alejoboga20@gmail.com>
2022-05-18 12:30:31 -0300

    Handle Task not found exception

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
 
+/* 
+DTO: Allows us to share data shapes between layers
+*/
 export class CreateTaskDto {
   @IsNotEmpty()
   title: string;
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 
 import { v4 as uuid } from 'uuid';
@@ -37,7 +37,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task not Found');
+    }
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
